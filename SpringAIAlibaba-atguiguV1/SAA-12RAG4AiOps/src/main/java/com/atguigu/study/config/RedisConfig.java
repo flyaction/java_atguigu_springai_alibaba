87f8bdd6e942ca3762f1c8ddf0262708c2801e95
action <xingdong1117@126.com>
2025-11-10 15:58:18 +0800

    feat:SAA-12RAG4AiDatabase - redis-cli --raw

--- SpringAIAlibaba-atguiguV1/SAA-12RAG4AiOps/src/main/java/com/atguigu/study/config/RedisConfig.java
+++ SpringAIAlibaba-atguiguV1/SAA-12RAG4AiOps/src/main/java/com/atguigu/study/config/RedisConfig.java
@@ -21,6 +21,7 @@ public class RedisConfig
     /**
      * RedisTemplate配置
      * redis序列化的工具配置类，下面这个请一定开启配置
+     * redis-cli --raw
      * 127.0.0.1:6379> keys *
      * 1) "ord:102"  序列化过
      * 2) "\xac\xed\x00\x05t\x00\aord:102"   野生，没有序列化过
